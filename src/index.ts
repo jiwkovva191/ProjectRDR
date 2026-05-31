807c79b17c6dc41070c9ae9efa2eb8053f46e377
jiwkovva19 <yankowadelyana@gmail.com>
2026-05-31 14:59:30 +0300

    changed the server port with env variable

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import express from 'express';
+import express, { Application } from 'express';
 //import userRoutes from "../routes/user.routes";
 import searchRoutes from "../routes/search.routes";
 import cors from "cors";
 import categoryRoutes from "../routes/category.routes";
 import skillRoutes from "../routes/skill.routes";
 
-const app = express();
-const PORT = 3001;
+const app: Application = express();
+const PORT = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(cors());
+
 
 //app.use(userRoutes);   ТОВА Е КОМЕНТИРАНО, ЗАЩОТО ОЩЕ НЯМАМЕ BACKEND ЗА USER
 app.use(searchRoutes);
