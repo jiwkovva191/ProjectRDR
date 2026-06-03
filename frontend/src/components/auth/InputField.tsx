639e86bcb0bf97f62651b847aba0ca8059875500
jiwkovva19 <yankowadelyana@gmail.com>
2026-06-03 21:14:37 +0300

    small change

--- frontend/src/components/auth/InputField.tsx
+++ frontend/src/components/auth/InputField.tsx
@@ -2,7 +2,7 @@ export const InputField = (props: any) => {
   return (
     <input
       type="text"
-      className="w-full border border-blue-200 rounded p-2"
+      className="w-full border border-blue-200 rounded p-2 mb-1"
       {...props}
     />
   );
