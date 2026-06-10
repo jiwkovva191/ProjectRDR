8ea857bc9ab024009e6fa2e1bb5824d2c6c8888b
jiwkovva19 <yankowadelyana@gmail.com>
2026-06-10 23:54:42 +0300

    wrapped the routes within AuthProvider and inserted login useAuth function inside LoginUser.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Layout from "./components/Layout.tsx";
 import Education from "./pages/Education.tsx";
@@ -11,37 +11,34 @@ import SkillPage from "./pages/SkillPage.tsx";
 import Programming from "./pages/Programming.tsx";
 import Design from "./pages/Design.tsx";
 import UserProfile from "./pages/UserProfile.tsx";
-
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 const App = () => {
-    return (
-
-<BrowserRouter>
-
-    <Routes>
-        <Route element={<Layout />} >
-
-
-            <Route path={'/'} element={<Home />}/>
-            <Route path={'/education'} element={<Education />}/>
-            <Route path={'/languages'} element={<Languages />}/>
-            <Route path={'/trades'} element={<Trades />}/>
-            <Route path={'/programming'} element={<Programming />}/>
-            <Route path={'/design'} element={<Design />}/>
-            <Route path={'/addSkill'} element={<AddSkill />}/>
-            <Route path={'/skills/:id'} element={<SkillPage />}></Route>
-            <Route path={'/users/:id/profile'} element={<UserProfile/>}></Route>
-
-        </Route>
-        <Route path={'/login'} element={<Login />}/>
-        <Route path={'/signup'} element={<SignUp />}/>
-
-
-    </Routes>
-</BrowserRouter>
-
-
-    )
-}
+  return (
+    // wrapping everything within AuthProvider every single one of the pages keeps the memory of the logged in user
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/education"} element={<Education />} />
+            <Route path={"/languages"} element={<Languages />} />
+            <Route path={"/trades"} element={<Trades />} />
+            <Route path={"/programming"} element={<Programming />} />
+            <Route path={"/design"} element={<Design />} />
+            <Route path={"/addSkill"} element={<AddSkill />} />
+            <Route path={"/skills/:id"} element={<SkillPage />}></Route>
+            <Route
+              path={"/users/:id/profile"}
+              element={<UserProfile />}
+            ></Route>
+          </Route>
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/signup"} element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
 
 export default App;
